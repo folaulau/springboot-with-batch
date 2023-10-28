83245c26010c0c18b1d803ecc5a131dd3f12a268
Folau Kaveinga <folaukaveinga@gmail.com>
2023-10-27 19:12:09 -0600

    setup

---
+++ src/main/java/com/folautech/batch/SpringbootWithBatchApplication.java
@@ -0,0 +1,13 @@
+package com.folautech.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folautech/batch/SpringbootWithBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folautech.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
